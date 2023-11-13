b54327369efc1ed94342c9c8557189874050fbf5
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2023-11-13 16:50:14 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,37 @@ const person = Person = {
     age : 
 
 }
+type UserName = string | number;
+let a: UserName = "hello ji";
+
+console.log(a);
+let n = 2;
+let m = 3;
+
+const func = (n: number, m: number): number => {
+  console.log("G");
+  return n * m;
+};
+console.log(func(n, m));
+
+const arr: number[] = [1, 2, 3, 4];
+const arr2: Array<string | number> = ["Shivam", "Arun", "Pawan", 123];
+
+arr2.forEach(i => {
+    i.toLocaleString();
+    console.log(i);
+    
+})
+
+//objects in typescript
+
+
+
+
+
+
+
+
 
 
 // Type -> 
