cf7ce98bc0f8a14c0dd7e82713b9423cd28f1bc4
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2023-11-13 16:15:44 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,18 @@ const ob1: Person = {
 interface Users {
     
 }
+
+type UserName = string | number;
+let a : UserName = "hello ji";
+
+console.log(a);
+let n =2;
+let m = 3;
+
+const func = (n: number , m:number): number => {
+    
+    console.log("G" );
+    return n*m;
+    
+}
+console.log(func(n,m));
